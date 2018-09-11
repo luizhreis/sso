c2d4c89c34d9d1a4ae71113aa4763907b8d3973b
Luiz <luizhreis.gris@gmail.com>
2018-09-11 19:18:48 -0300

    Alteração de parâmetros do process

--- process.c
+++ process.c
@@ -1,13 +1,14 @@
 struct Process{
     int pid;
+    int ppid;
     int state;
-    int pidFather;
     float quantum;
 };
 
-struct Proccess *createProcess(int pid, float quantum){
+struct Proccess *createProcess(int pid, int ppid, float quantum){
     struct Process *p = (struct Process*)malloc(sizeof(struct Process));
     p->pid = pid;
+    p->ppid = ppid;
     p->state = 0;
     p->quantum = quantum;
 };
