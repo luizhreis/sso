c94228e0b5c2424354ab64d7edb5a36e429c9b4d
Luiz <luizhreis.gris@gmail.com>
2018-09-12 14:03:32 -0300

    Testes do código

--- sso.c
+++ sso.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 
 #include "processManager.h"
+#include "fifoQueues.h"
+#include "process.h"
 
-// Driver Program to test anove functions
+const int maxProcess = 1024;
+
 int main(){
-    struct Queue *q = createQueue();
-    struct Queue *rrQueue01 = createQueue();
-    struct Queue *rrQueue02 = createQueue();
-    struct Queue *rrQueue03 = createQueue();
-    struct Queue *cpuBoundedQueue01 = createQueue();
-    struct Queue *cpuBoundedQueue02 = createQueue();
-    struct Queue *ioBoundedQueue = createQueue();
-
     struct Process *process = createProcess(100, 0, 0, 1.5);
 
     struct ProcessManager *manager = createProcessManager();
     manager->highPriorityQueue = createQueue();
+    manager->lowPriorityQueue = createQueue();
+    manager->ioQueue = createQueue();
 
     printf("lista vazia: %d\n", isEmpty(manager->highPriorityQueue));
     enQueue(manager->highPriorityQueue, 10);
@@ -39,7 +36,7 @@ int main(){
     printf("PID: %d, QUANTUM: %f\n", process->pid, process->quantum);
 
     struct Node *n = deQueue(manager->highPriorityQueue);
-    if (n != NULL)
-      printf("Dequeued item is %d\n", n->data);
+    if(n != NULL)
+        printf("Dequeued item is %d\n", n->data);
     return 0;
 }
