44bbe3ac1e8cc2d9736f947b318ce8bb78616856
Luiz <luizhreis.gris@gmail.com>
2018-09-18 17:00:19 -0300

    Usando lista de processos

--- sso.c
+++ sso.c
@@ -13,15 +13,29 @@ int main(){
     manager->lowPriorityQueue = createQueue();
     manager->ioQueue = createQueue();
 
+    // struct Process *processList = (struct Process*)malloc(sizeof(struct Process)*maxProcess);
+    struct Process *(processList[maxProcess]);
+    struct Process *tmpProc;
+    struct Process *teste;
+
     // struct Process *proc1 = generateProcess(manager->nextPid, 0, 0, 10);
     printf("PID ATUAL: %d", manager->nextPid);
-    struct Process *proc1 = createProcess(manager->nextPid, 0, 0, 10);
+    tmpProc = createProcess(manager->nextPid, 0, 0, 10);
+    processList[manager->nextPid] = tmpProc;
+
+    teste = processList[0];
+    printf("\nTESTE: %d", teste->pid);
+
     manager->nextPid = generateNextPid(manager->nextPid);
     printf("PID ATUAL: %d", manager->nextPid);
-    enQueue(manager->highPriorityQueue, proc1->pid);
-    struct Process *proc2 = createProcess(manager->nextPid, 0, 0, 10);
+    enQueue(manager->highPriorityQueue, tmpProc->pid);
+    tmpProc = createProcess(manager->nextPid, 0, 0, 10);
+    processList[manager->nextPid] = tmpProc;
     manager->nextPid = generateNextPid(manager->nextPid);
-    enQueue(manager->highPriorityQueue, proc2->pid);
+    enQueue(manager->highPriorityQueue, tmpProc->pid);
+
+    teste = processList[1];
+    printf("\nTESTE: %d", teste->pid);
 
     printf("lista vazia: %d\n", isEmpty(manager->highPriorityQueue));
     enQueue(manager->highPriorityQueue, 10);
@@ -41,8 +55,8 @@ int main(){
     enQueue(manager->highPriorityQueue, 50);
     show(manager->highPriorityQueue);
 
-    printf("PID: %d, QUANTUM: %f\n", proc1->pid, proc1->quantum);
-    printf("PID: %d, QUANTUM: %f\n", proc2->pid, proc2->quantum);
+    // printf("PID: %d, QUANTUM: %f\n", proc1->pid, proc1->quantum);
+    // printf("PID: %d, QUANTUM: %f\n", proc2->pid, proc2->quantum);
 
     struct Node *n = deQueue(manager->highPriorityQueue);
     if(n != NULL)
